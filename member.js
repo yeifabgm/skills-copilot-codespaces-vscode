bebffa52902ac219ff6a7b1fc06d531b8dbeed0f
Yeison Gutierrez <yeifab.gutierrez@gmail.com>
2023-10-26 11:13:35 +1030

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMenber() {
+  var skills = document.getElementById("skills");
+  var member = document.getElementById("member");
+  var skillsBtn = document.getElementById("skillsBtn");
+  var memberBtn = document.getElementById("memberBtn");
+
+  skills.style.display = "none";
+  member.style.display = "block";
+  skillsBtn.style.backgroundColor = "#fff";
+  memberBtn.style.backgroundColor = "#e6e6e6";
+}
